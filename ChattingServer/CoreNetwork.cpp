0f125b0ced908ada04d8350d317847e0c74fb00d
성원 정 <tyhg56@naver.com>
2025-05-20 20:18:48 +0900

    feat : " IOCP 핸들 생성, accept 스레드 함수, worker 스레드 함수 추가, Listen 소켓 생성 및 바인드 "
    CoreNetwork
     - _HCP ( IOCP 핸들 )
    
     - _listenSocket ( Listen 소켓 )
    
     - Start 함수 추가 ( 서버 시작 진입점 )
      - 리슨 소켓 생성 및 바인딩
      - IOCP Port 생성
      - AcceptThreadProc 추가 ( accept 스레드 함수 )
      - WorkerThreadProc 추가 ( worker 스레드 함수 )
    
     - AcceptThreadProc ( 서버 accept 처리 전용 스레드 )
    
     - WorkerThreadProc ( IOCP worker 스레드 )
    
     - 생성자 수정
      - WSAStartup 호출

--- ChattingServer/CoreNetwork.cpp
+++ ChattingServer/CoreNetwork.cpp
@@ -2,9 +2,80 @@
 
 CoreNetwork::CoreNetwork()
 {
-
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		DWORD error = WSAGetLastError();
+		std::cout << "WSAStartup failed : " << error << std::endl;
+		return;
+	}
 }
 
 CoreNetwork::~CoreNetwork()
 {
+}
+
+bool CoreNetwork::Start(const WCHAR* openIP, int port)
+{
+	// 府郊 家南 积己
+	_listenSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (_listenSocket == INVALID_SOCKET)
+	{
+		std::cout << "socket failed" << std::endl;
+		return false;
+	}
+
+	// 官牢靛
+	SOCKADDR_IN serverAddr;
+	memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(port);
+	InetPton(AF_INET, openIP, &serverAddr.sin_addr);
+
+	int bindResult = bind(_listenSocket, (SOCKADDR*)&serverAddr, sizeof(SOCKADDR_IN));
+	if (bindResult == SOCKET_ERROR)
+	{
+		std::cout << "bind failed" << std::endl;
+		return false;
+	}
+
+	// 府郊
+	int listenResult = listen(_listenSocket, SOMAXCONN);
+	if (listenResult == SOCKET_ERROR)
+	{
+		std::cout << "listen failed" << std::endl;
+		return false;
+	}
+
+	// IOCP Port甫 积己茄促.
+	_HCP = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
+	if (_HCP == NULL)
+	{
+		std::cout << "CreateIoCompletionPort failed" << std::endl;
+		return false;
+	}
+
+	// accept 胶饭靛 积己
+	HANDLE hAcceptThread = (HANDLE)_beginthreadex(NULL, 0, AcceptThreadProc, this, 0, NULL);
+	CloseHandle(hAcceptThread);
+
+	// IOCP 况目 胶饭靛 积己
+	SYSTEM_INFO SI;
+	GetSystemInfo(&SI);
+	
+	for(int i=0;i<(int)SI.dwNumberOfProcessors;i++)
+	{
+		HANDLE hWorkerThread = (HANDLE)_beginthreadex(NULL, 0, WorkerThreadProc, this, 0, NULL);
+		CloseHandle(hWorkerThread);
+	}
+}
+
+unsigned __stdcall CoreNetwork::AcceptThreadProc(void* argument)
+{
+	return 0;
+}
+
+unsigned __stdcall CoreNetwork::WorkerThreadProc(void* argument)
+{
+	return 0;
 }
--- ChattingServer/CoreNetwork.h
+++ ChattingServer/CoreNetwork.h
@@ -1,9 +1,32 @@
 #pragma once
+#pragma comment(lib,"ws2_32")
+
+#include<WinSock2.h>
+#include<WS2tcpip.h>
+#include<process.h>
+#include<sysinfoapi.h>
+#include<iostream>
+
+using namespace std;
 
 class CoreNetwork
 {
 public:
 	CoreNetwork();
 	~CoreNetwork();
-private:	
+private:
+	// IOCP 핸들 
+	HANDLE _HCP;
+
+	// 리슨 소켓
+	SOCKET _listenSocket;
+
+	// 서버 시작
+	bool Start(const WCHAR* openIP, int port);
+
+	// Accept 전용 스레드 함수
+	static unsigned __stdcall AcceptThreadProc(void* argument);
+
+	// Worker 전용 스레드 함수
+	static unsigned __stdcall WorkerThreadProc(void* argument);
 };
