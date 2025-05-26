1e5decdc41523e89cbb455ba8e6af4954bc766ac
성원 정 <tyhg56@naver.com>
2025-05-26 16:09:02 +0900

    Fix : " 미리 컴파일된 헤더 추가 "
    솔루션 수정
     - 미리 컴파일된 헤더 사용하도록 수정
     - pch.h , pch.cpp 추가

---
+++ ChattingServer/pch.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include<WinSock2.h>
+#include<WS2tcpip.h>
+
+#include<process.h>
+
+#include<iostream>
+#include<queue>
+#include<list>
+#include<array>
+
+#include<cstdint>
+
+#include<cstring>
+
+using namespace std;
+
