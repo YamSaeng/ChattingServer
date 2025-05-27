4c10a2c5758e262f84d49cb34d33e9e6f8acdbd8
성원 정 <tyhg56@naver.com>
2025-05-27 18:45:03 +0900

    feat: 더미 클라이언트 메뉴 출력문 추가
    - 콘솔에 유니코드 한글 메뉴 출력문 추가
    - 채팅 더미 개수 미설정 시 경고 메시지 출력

--- DummyClient/main.cpp
+++ DummyClient/main.cpp
@@ -2,5 +2,30 @@
 
 int main()
 {
+	bool isDummyCountSet = false;
+	short dummyClientMenuSelectNum = 0;
+
+	_setmode(_fileno(stdout), _O_U16TEXT);
+
+	while (1)
+	{
+		wcout << L"채팅 더미 클라이언트 시작" << endl;
+		wcout << L"1. 채팅 더미 개수 설정" << endl;
+		wcout << L"2. 채팅 더미 서버 접속 시작" << endl;
+		wcout << L"선택 : ";
+		cin >> dummyClientMenuSelectNum;
+
+		if (dummyClientMenuSelectNum == 2)
+		{
+			if (isDummyCountSet == false)
+			{
+				wcout << L"채팅 더미 개수를 설정하고 접속하세요" << endl;
+				wcout << L"====================================" << endl << endl;
+			}
+		}
+
+		Sleep(1000);
+	}
+
 	return 0;
 }
