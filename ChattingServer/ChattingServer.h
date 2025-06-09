14d3eab3b9ea73069a5343c49740cfefa6fef4c5
성원 정 <tyhg56@naver.com>
2025-06-09 10:42:21 +0900

    feat: 패킷 처리 함수를 함수 포인터 배열에 할당
    - PACKET_PROC_COUNT 추가 ( packet 처리 함수 개수 상수 값 )
    - (ChattingServer::*packetProc[PACKET_PROC_COUNT](__int64 sessionId, Packet* packet); 추가
     - packet 처리 함수를 저장해둘 함수 포인터 배열
    - 생성자에서 함수 포인터 저장

--- ChattingServer/ChattingServer.h
+++ ChattingServer/ChattingServer.h
@@ -2,12 +2,18 @@
 
 #include"CoreNetwork.h"
 
+#define PACKET_PROC_COUNT 5
+
 class ChattingServer : public CoreNetwork
 {
 public:	
-
 	ChattingServer();
-	~ChattingServer();
-
-	virtual void OnClientJoin(Session* newSession) override;
+	~ChattingServer();	
+private:
+	void (ChattingServer::*packetProc[PACKET_PROC_COUNT])(__int64 sessionId, Packet* packet);
+	void PacketProcReqChat(__int64 sessionId, Packet* packet);
+public:
+	void OnClientJoin(Session* newSession) override;
+	void OnRecv(__int64 sessionId, Packet* packet) override;
+	void OnClientLeave(Session* leaveSession) override;
 };
