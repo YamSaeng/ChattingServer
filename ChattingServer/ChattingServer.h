5139696d9a979cf85200c74cce235bb90ee147b5
성원 정 <tyhg56@naver.com>
2025-06-19 20:09:08 +0900

    feat: 채팅서버 패킷 처리 방식 변경 ( UpdateThread에서 처리 )
    ChattingServer
     - Job 구조체 추가 ( packet과 sessionId를 담아둘 구조체 )
     - _jobPool 추가 ( job 메모리 풀 )
     - _jobQueue 추가 ( 서버에서 받은 Job으로 한번 포장해서 넣어둠 )
     - _jobQueueLock 추가 ( _jobQueue 동기화용 CriticalSection )
    
     - UpdateThreadProc 추가 ( _job처리 )
    
     - OnRecv() 수정
      - JobPool에서 할당받고 새로 Packet을 할당한 후 JobQueue에 넣은 뒤 UpdateThread를 깨워서 패킷을 처리할 수 있도록함

--- ChattingServer/ChattingServer.h
+++ ChattingServer/ChattingServer.h
@@ -2,7 +2,15 @@
 
 #include"CoreNetwork.h"
 
+#include"../ObjectPool.h"
+
 #define MAX_PROTOCOL_COUNT 50
+
+struct Job
+{	
+	__int64 sessionId;
+	Packet* packet;
+};
 
 class ChattingServer : public CoreNetwork
 {
@@ -10,10 +18,20 @@ public:
 	ChattingServer();
 	~ChattingServer();	
 private:
+	HANDLE _hUpdateThread;
+	HANDLE _hUpdateWakeEvent;
+
+	ObjectPool<Job>* _jobPool;
+
+	queue<Job*> _jobQueue;
+	CRITICAL_SECTION _jobQueueLock;
+
 	void (ChattingServer::*packetProc[MAX_PROTOCOL_COUNT])(__int64 sessionId, Packet* packet);
 	void PacketProcReqChat(__int64 sessionId, Packet* packet);	
 	
 	Packet* MakePacketProcResChat(string& chatMessage);
+
+	static unsigned __stdcall UpdateThreadProc(void* argument);	
 public:
 	void OnClientJoin(Session* newSession) override;
 	void OnRecv(__int64 sessionId, Packet* packet) override;
